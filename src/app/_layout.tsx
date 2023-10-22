ef0e9ab60dc42cad25f0a782524ba6d9253c5c03
emersonv25 <emersondejesussantos@hotmail.com>
2023-10-22 17:27:03 -0300

    Fix: Web

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,11 @@ import {
 } from '@tanstack/react-query'
 import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
